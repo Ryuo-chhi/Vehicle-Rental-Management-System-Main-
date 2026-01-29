22beab6971b6aecac0802a5c14d362dcd8121281
thangsaoly <lovepapa987@gmail.com>
2026-01-29 09:00:45 +0700

    Add Vehicle Class Implementation to the project.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,25 @@
 public class Vehicle {
+    int vehicleId;
+    String vehiclePower;
+    String vehicleClass;
+    String vehicleModel;
+    boolean isAvailable;
+
+    static int nextVehicleId = 1; // this var keeps track of the next vehicle ID to assign. why static? because it's shared among all instances in order to ensure unique IDs.
+
+    public Vehicle(String vehiclePower, String vehicleClass, String vehicleModel) {
+        this.vehicleId = nextVehicleId++;
+        this.vehiclePower = vehiclePower;
+        this.vehicleClass = vehicleClass;
+        this.vehicleModel = vehicleModel;
+        this.isAvailable = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle [id=" + vehicleId + ", power=" + vehiclePower + ", vehicleClass=" + vehicleClass + ", model="
+                + vehicleModel
+                + ", isAvailable=" + isAvailable + "]";
+    }
+
 }
