60dfa71073f49f7782321270873a8a2e362a244c
thangsaoly <lovepapa987@gmail.com>
2026-01-29 09:23:16 +0700

    Add Customer class to project.

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,20 @@
 public class Customer {
+    int customerId;
+    String customerIdCard; // national id card number, passport number, driver's license number, etc.
+    String customerName; 
+    String customerPhone;
+
+    static int nextCustomerId = 1; // this var keeps track of the next customer ID to assign. why static? because it's shared among all instances in order to ensure unique IDs.
+
+    public Customer(String customerIdCard, String customerName, String customerPhone) {
+        this.customerId = nextCustomerId++;
+        this.customerIdCard = customerIdCard;
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [id=" + customerId + ", name=" + customerName + ", phone=" + customerPhone + "]";
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,15 +1,15 @@
 public class Vehicle {
     int vehicleId;
-    String vehiclePower;
-    String vehicleClass;
-    String vehicleModel;
-    boolean isAvailable;
+    String vehiclePowerSource; // "gasoline", "electric", "hybrid"
+    String vehicleClass; // "sedan", "SUV", "truck", etc.
+    String vehicleModel; // "Toyota Camry", "Ford F-150", etc.
+    boolean isAvailable; 
 
     static int nextVehicleId = 1; // this var keeps track of the next vehicle ID to assign. why static? because it's shared among all instances in order to ensure unique IDs.
 
-    public Vehicle(String vehiclePower, String vehicleClass, String vehicleModel) {
+    public Vehicle(String vehiclePowerSource, String vehicleClass, String vehicleModel) {
         this.vehicleId = nextVehicleId++;
-        this.vehiclePower = vehiclePower;
+        this.vehiclePowerSource = vehiclePowerSource;
         this.vehicleClass = vehicleClass;
         this.vehicleModel = vehicleModel;
         this.isAvailable = true;
@@ -17,7 +17,7 @@ public class Vehicle {
 
     @Override
     public String toString() {
-        return "Vehicle [id=" + vehicleId + ", power=" + vehiclePower + ", vehicleClass=" + vehicleClass + ", model="
+        return "Vehicle [id=" + vehicleId + ", power=" + vehiclePowerSource + ", vehicleClass=" + vehicleClass + ", model="
                 + vehicleModel
                 + ", isAvailable=" + isAvailable + "]";
     }
