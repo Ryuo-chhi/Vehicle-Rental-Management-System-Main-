31abc311b6714e6cd9b01c676f9f411ae7c99c74
thangsaoly <lovepapa987@gmail.com>
2026-03-07 11:58:48 +0700

    add keyword abstract to Staff.java and modify can method.

--- src/user/Staff.java
+++ src/user/Staff.java
@@ -2,7 +2,7 @@ package user;
 
 import java.util.Objects;
 
-public class Staff implements IStaff {
+public abstract class Staff implements IStaff {
     /* ====== Fields (Encapsulation) ====== */
     private int staffId;
     private String name;
@@ -16,11 +16,8 @@ public class Staff implements IStaff {
 
     private static int staffCount = 0;
 
-    /*====== user.Staff Permissions (default: no access — override in subclasses) ====== */
-    @Override
-    public boolean can(String action) {
-        return false;
-    }
+    /*====== user.Staff Permissions (must be defined by each role) ====== */
+    public abstract boolean can(String action);
 
     /*====== Register ====== */
     public Staff(String name, String role, double salary, String username, String password) {
